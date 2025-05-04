2868f02e6f12305cf55758eaa3c35eab44a90df7
Surya07091997 <tejandd17@gmail.com>
2025-05-04 09:49:41 -0400

    Add files via upload

--- utils/cardsData.js
+++ utils/cardsData.js
@@ -1,1 +1,34 @@
+const cards = [
+    {
+      name: "Cristin Ababa",
+      title: "DEFAULT APPRAISALX1",
+      position: "Accounting Manager",
+      dateRange: "12-11-2054 to 12-10-2055",
+      dueDate: "10-31-2054",
+      score: "0.00",
+      status: "Current",
+      overdue: true,
+    },
+    {
+      name: "Cristin Ababa",
+      title: "90 DAY REVIEW",
+      position: "Accounting Manager",
+      dateRange: "12-17-2024 to 12-25-2024",
+      dueDate: "12-27-2024",
+      score: "2.50",
+      status: "Overdue",
+      overdue: true,
+    },
+    {
+      name: "Cristin Ababa",
+      title: "NCF",
+      position: "Accounting Manager",
+      dateRange: "",
+      dueDate: "",
+      score: "",
+      status: "Current",
+      overdue: false,
+    },
+  ];
 
+  export default cards;
---
+++ utils/mockData.js
@@ -0,0 +1,9 @@
+export const appraisalRecords = {
+  '1': {
+    score: null,
+    comment: '',
+    summaryComment: '',
+    summaryDate: '',
+    status: 'Pending'
+  }
+};
